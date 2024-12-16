69574f28de858dc7b2ac04430fc838e1af4c0db9
NailecMC <125212178+naileclevrai@users.noreply.github.com>
2024-12-16 16:41:22 +0100

    ajouter repoces

--- Première scéance exercice 3.py
+++ Première scéance exercice 3.py
@@ -11,3 +11,10 @@ print(type(e))
 e="quel est ton prenom"
 print(type(e))
 # exercice sur les int, float, str pour decouvrir ce que c'est
+
+#<class 'int'> = int = integer = nombre entier
+#<class 'int'> = //
+#<class 'float'> = floating number = Nombre flotant = Nombre aproximatif
+#<class 'float'> = //
+#<class 'str'> = string = texte
+#<class 'str'> = //
