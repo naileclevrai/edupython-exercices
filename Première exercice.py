f5b50efa822bd3793f2f786fcddec7976737ccb0
NailecMC <125212178+naileclevrai@users.noreply.github.com>
2024-12-16 15:47:29 +0100

    Exercice 1 ezz

---
+++ Première exercice.py
@@ -0,0 +1,14 @@
+# Scéance 1 : Première exercice
+# git on 16/12/2024
+a=2
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print("La somme de a et de b est ", a+b)
+print("La différence de a et de b est", a-b)
+print("Le produit de a par b est", a*b)
+print("Le quoptient de a par b est", a/b)
+print("Le résultat de a exsposant b est", a**b)
