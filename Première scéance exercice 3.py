c0b5153c7a522962a2ab0590a6fadb114de07907
NailecMC <125212178+naileclevrai@users.noreply.github.com>
2024-12-16 16:39:49 +0100

    exercice 3

---
+++ Première scéance exercice 3.py
@@ -0,0 +1,13 @@
+a=2
+print(type(a))
+b = -5
+print(type(b))
+c=10.3
+print(type(c))
+d=20/3
+print(type(d))
+e="Bonjour"
+print(type(e))
+e="quel est ton prenom"
+print(type(e))
+# exercice sur les int, float, str pour decouvrir ce que c'est
